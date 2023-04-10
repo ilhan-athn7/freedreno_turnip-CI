24ba93dd611bd71d63638040b3390eb6e5f92aae
İlhan Atahan <80551220+ilhan-athn7@users.noreply.github.com>
2023-04-10 23:34:00 +0300

    Set selinux contexts to work with KernelSU
    Possibly compatible with KernelSU now.

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -156,7 +156,9 @@ EOF
 
 
 cat <<EOF >"customize.sh"
-set_perm \$MODPATH/$p1/vulkan.adreno.so 0 0 0644
+set_perm_recursive \$MODPATH/system 0 0 755 u:object_r:system_file:s0
+set_perm_recursive \$MODPATH/system/vendor 0 2000 755 u:object_r:vendor_file:s0
+set_perm \$MODPATH/$p1/vulkan.adreno.so 0 0 0644 u:object_r:same_process_hal_file:s0
 EOF
 
 
