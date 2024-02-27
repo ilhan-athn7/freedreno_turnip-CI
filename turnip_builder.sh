d15e515071b251a30f824996a745346c09e84757
İlhan Atahan <80551220+ilhan-athn7@users.noreply.github.com>
2024-02-27 09:58:35 +0300

    Update NDK from r26b to r26c

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -6,7 +6,7 @@ nocolor='\033[0m'
 deps="meson ninja patchelf unzip curl pip flex bison zip"
 workdir="$(pwd)/turnip_workdir"
 magiskdir="$workdir/turnip_module"
-ndkver="android-ndk-r26b"
+ndkver="android-ndk-r26c"
 sdkver="31"
 mesasrc="https://gitlab.freedesktop.org/mesa/mesa/-/archive/main/mesa-main.zip"
 clear
