3dc469c70fcbf629083098722625887d481430fb
Shankar Vallabhan <shankarmanu@protonmail.com>
2024-10-20 16:57:16 +0530

    fix: Add -Dstrip=true and update Meson command to resolve deprecation warning
    - Added the option -Dstrip=true to strip debugging symbols from the binaries, reducing their size for production builds.
    - Changed the Meson command from 'meson' to 'meson setup' to follow best practices and avoid deprecation warnings in future versions.
    
    Signed-off-by: Shankar Vallabhan (v3kt0r-87) <shankarmanu@protonmail.com>

--- turnip_builder.sh
+++ turnip_builder.sh
@@ -86,7 +86,7 @@ endian = 'little'
 EOF
 
 	echo "Generating build files ..." $'\n'
-	meson build-android-aarch64 --cross-file "$workdir"/mesa-main/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=$sdkver -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dvulkan-beta=true -Dfreedreno-kmds=kgsl -Db_lto=true &> "$workdir"/meson_log
+	meson setup build-android-aarch64 --cross-file "$workdir"/mesa-main/android-aarch64 -Dbuildtype=release -Dplatforms=android -Dplatform-sdk-version=$sdkver -Dandroid-stub=true -Dgallium-drivers= -Dvulkan-drivers=freedreno -Dvulkan-beta=true -Dfreedreno-kmds=kgsl -Db_lto=true -Dstrip=true &> "$workdir"/meson_log
 
 	echo "Compiling build files ..." $'\n'
 	ninja -C build-android-aarch64 &> "$workdir"/ninja_log
